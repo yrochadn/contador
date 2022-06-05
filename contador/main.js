5e705ee015d11024e6cc067c11583403e7ffcddd
Yuri Rocha <yurirocha1995@gmail.com>
2022-06-05 18:56:07 -0400

    first commit

---
+++ contador/main.js
@@ -0,0 +1,23 @@
+let count = 0;
+const contador = document.getElementById('currentNumber');
+const text = document.getElementsByTagName('button')
+
+
+
+
+
+function incrementar() {
+    count++
+    contador.innerHTML = count;
+    const color = text.adicionar.innerHTML
+    color.style='color: blue'
+
+
+}
+
+
+function decrementar() {
+    count--
+    contador.innerHTML = count
+
+}
